453104b6dfaa967710672ef6e205a7ebbddc1653
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-25 12:34:25 +0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="white-mode">
+      <nav>
+       <p>NavBar</p>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
